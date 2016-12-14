20615e13aef4cbdf2526482bb7db893c3d927dd5
Renan Birck <renan.ee.ufsm@gmail.com>
2016-12-14 21:45:52 -0200

    Move stuff to outside __init__.py

--- pyibge/extra_routines.py
+++ pyibge/extra_routines.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-#
-# pyIBGE: A module to access data from the Brazilian Institute of Geography and Statistics (IBGE)
-# (c) 2016 Renan Birck Pinheiro [renan.birck.pinheiro@gmail.com]
-
-from .extra_routines import state_to_id
-
-def test():
-    return("hello world from pyibge")
-
 def state_to_id(state):
     """Convert a state to its ID as used by the IBGE databases. """
     
