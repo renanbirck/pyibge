3b51a2f49d0c49b41957ff3f35e190af18970fea
Renan Birck <renan.ee.ufsm@gmail.com>
2016-12-14 20:42:56 -0200

    minimal skeleton

--- pyibge/__init__.py
+++ pyibge/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
+#
+# pyIBGE: A module to access data from the Brazilian Institute of Geography and Statistics (IBGE)
+# (c) 2016 Renan Birck Pinheiro [renan.birck.pinheiro@gmail.com]
 
+
+def test():
+    return("hello world from pyibge")
