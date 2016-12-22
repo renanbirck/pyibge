d54d622faeb0b3a32db768a83a2a52203c7737e9
Renan Birck <renan.ee.ufsm@gmail.com>
2016-12-22 21:31:33 -0200

    begin the query system.

--- pyibge/query.py
+++ pyibge/query.py
@@ -13,14 +13,45 @@ class IBGEQuery():
     """ The class that represents a query. """
 
     def __init__(self, table_ID=None, period='last',
-                 variables='allxp'):
+                 variables='allxp', level='n1', sublevel='all',
+                 classification=None, **kwargs):
         if 0 < table_ID < 9999:
             self.table_ID = table_ID
         else:
             raise ValueError("Table ID must be between 0 and 9999.")
-        pass
+
 
         self.period = period
         self.variables = variables
+        self.level = level
+        self.classification = classification
 
-    pass
+        # kwargs are used for defining the formatting.
+
+        # The 'f' parameter.
+        try:
+            self.result_format = kwargs['format'].lower
+        except KeyError:    # No result format was given
+            self.result_format = 'a'
+
+        if self.result_format and self.result_format not in ['c', 'n', 'u', 'a']:
+            raise ValueError("The result format must be one of 'c', 'n', 'u' or 'a'.")
+        
+        # The 'd' parameter.
+
+        try:
+            self.digits = kwargs['num_digits'].lower
+        except KeyError:
+            self.digits = 's'
+
+        if self.digits and self.digits not in ['s', 'm', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            raise ValueError("The digits must be 0-9, 'm' (maximum digits available) or 's' (default).")
+
+        # The 'h' parameter
+
+        try:
+            self.header = kwargs['get_header']
+        except KeyError:
+            self.header = True
+
+        
